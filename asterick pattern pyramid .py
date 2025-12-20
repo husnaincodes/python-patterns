300c5dc22d8fdb4aa2363ddd7a2e0758b748707b
Husnain <Husnain.tayab@hotmail.com>
2025-12-20 23:24:18 +0500

    Full pyramid

--- asterick pattern pyramid .py
+++ asterick pattern pyramid .py
@@ -4,8 +4,7 @@ rows = int(input("Enter the number : "))
 for i in  range(1,rows+1):
 
     print(" "*(rows-i)+"*"*(2*i-1))
-
-
+    
 for i in range(rows,0 , -1):
 
     print(" "*(rows - i)+"*"*(2*i-1))
