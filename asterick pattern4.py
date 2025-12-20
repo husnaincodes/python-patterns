2a77ebae64ae1a9b8fc61528e14ef8ddb7b59b19
Husnain <Husnain.tayab@hotmail.com>
2025-12-20 23:26:39 +0500

    Asterick Pattern

--- asterick pattern4.py
+++ asterick pattern4.py
@@ -4,6 +4,7 @@ rows = int(input("Enter the number of Pattern : "))
 for i in range (1 , rows+1):
     print("*"*i +" "*(2*(rows-i) )+"*"*i)
 
+
 #lower part
 for i in range (rows ,0 ,-1):
     print("*"*i +" "*(2*(rows-i) )+"*"*i)
