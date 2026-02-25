a0429acb1cc6fd26919c6defde4dc8233c01b198
Husnain <Husnain.tayab@hotmail.com>
2026-02-25 18:24:21 +0500

    Asterick Pattern

--- asterick pattern6.py
+++ asterick pattern6.py
@@ -1,7 +1,8 @@
-
 rows =  int(input("Enter the number of Pattern: "))
 for i in range(rows):
+
     if  i == 0 or i == rows-1:
         print("*"*rows)
+        
     else:
         print("*"+" "*(rows-2)+"*")
