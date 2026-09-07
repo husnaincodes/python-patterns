c134102e91d30b92a531a6bf48fe9c54284549a8
husnaincodes <Husnain.tayab@hotmail.com>
2026-09-07 18:43:27 +0500

    Add Spacing

--- abc pattern.py
+++ abc pattern.py
@@ -1,6 +1,8 @@
 rows = int(input("Enter number of rows: "))
 
 for i in range(rows):
+
     for j in range(i + 1):
-        print(chr(65 + j), end=" ")  
+        print(chr(65 + j), end=" ")
+        
     print()
